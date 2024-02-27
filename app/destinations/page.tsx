bb17172c993d09859bbaea6861594b7fa77cd7ec
acsalameh <acsalameh@gmail.com>
2024-02-27 13:36:33 +0200

    destinations logic sleected added

--- app/destinations/page.tsx
+++ app/destinations/page.tsx
@@ -1,9 +1,18 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./dist/destinations.css";
 import Image from "next/image";
 
 import destination from "../assets/destination.png";
+
+type StaticImageData = string;
+interface Destination {
+  id: number;
+  name: string;
+  continent: string;
+  country: string;
+  image: StaticImageData;
+}
 
 export default function Destinations() {
   const [destinations, setDestinations] = useState([
@@ -16,166 +25,216 @@ export default function Destinations() {
     },
     {
       id: 2,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      name: "France",
+      continent: "Europe",
+      country: "France",
       image: destination,
     },
     {
       id: 3,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      name: "Australia",
+      continent: "Oceania",
+      country: "Australia",
       image: destination,
     },
     {
       id: 4,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      name: "Brazil",
+      continent: "South America",
+      country: "Brazil",
       image: destination,
     },
     {
       id: 5,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      name: "Egypt",
+      continent: "Africa",
+      country: "Egypt",
       image: destination,
     },
     {
       id: 6,
-      name: "Lebanon",
+      name: "Japan",
       continent: "Asia",
-      country: "Lebanon",
+      country: "Japan",
       image: destination,
     },
     {
-      id: 1,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 7,
+      name: "Italy",
+      continent: "Europe",
+      country: "Italy",
       image: destination,
     },
     {
-      id: 2,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 8,
+      name: "New Zealand",
+      continent: "Oceania",
+      country: "New Zealand",
       image: destination,
     },
     {
-      id: 3,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 9,
+      name: "Argentina",
+      continent: "South America",
+      country: "Argentina",
       image: destination,
     },
     {
-      id: 4,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 10,
+      name: "South Africa",
+      continent: "Africa",
+      country: "South Africa",
       image: destination,
     },
     {
-      id: 5,
-      name: "Lebanon",
+      id: 11,
+      name: "Thailand",
       continent: "Asia",
-      country: "Lebanon",
+      country: "Thailand",
       image: destination,
     },
     {
-      id: 6,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 12,
+      name: "Spain",
+      continent: "Europe",
+      country: "Spain",
       image: destination,
     },
     {
-      id: 1,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 13,
+      name: "Fiji",
+      continent: "Oceania",
+      country: "Fiji",
       image: destination,
     },
     {
-      id: 2,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 14,
+      name: "Chile",
+      continent: "South America",
+      country: "Chile",
       image: destination,
     },
     {
-      id: 3,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 15,
+      name: "Morocco",
+      continent: "Africa",
+      country: "Morocco",
       image: destination,
     },
     {
-      id: 4,
-      name: "Lebanon",
+      id: 16,
+      name: "India",
       continent: "Asia",
-      country: "Lebanon",
+      country: "India",
       image: destination,
     },
     {
-      id: 5,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 17,
+      name: "Germany",
+      continent: "Europe",
+      country: "Germany",
       image: destination,
     },
     {
-      id: 6,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 18,
+      name: "Samoa",
+      continent: "Oceania",
+      country: "Samoa",
       image: destination,
     },
     {
-      id: 6,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 19,
+      name: "Peru",
+      continent: "South America",
+      country: "Peru",
       image: destination,
     },
     {
-      id: 6,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 20,
+      name: "Kenya",
+      continent: "Africa",
+      country: "Kenya",
       image: destination,
     },
     {
-      id: 6,
-      name: "Lebanon",
+      id: 21,
+      name: "Maldives",
       continent: "Asia",
-      country: "Lebanon",
+      country: "Maldives",
       image: destination,
     },
     {
-      id: 6,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 22,
+      name: "Greece",
+      continent: "Europe",
+      country: "Greece",
       image: destination,
     },
     {
-      id: 6,
-      name: "Lebanon",
-      continent: "Asia",
-      country: "Lebanon",
+      id: 23,
+      name: "Vanuatu",
+      continent: "Oceania",
+      country: "Vanuatu",
       image: destination,
     },
     {
-      id: 6,
-      name: "Lebanon",
+      id: 24,
+      name: "Colombia",
+      continent: "South America",
+      country: "Colombia",
+      image: destination,
+    },
+    {
+      id: 25,
+      name: "Tanzania",
+      continent: "Africa",
+      country: "Tanzania",
+      image: destination,
+    },
+    {
+      id: 26,
+      name: "Vietnam",
       continent: "Asia",
-      country: "Lebanon",
+      country: "Vietnam",
+      image: destination,
+    },
+    {
+      id: 27,
+      name: "Netherlands",
+      continent: "Europe",
+      country: "Netherlands",
+      image: destination,
+    },
+    {
+      id: 28,
+      name: "Tonga",
+      continent: "Oceania",
+      country: "Tonga",
+      image: destination,
+    },
+    {
+      id: 29,
+      name: "Ecuador",
+      continent: "South America",
+      country: "Ecuador",
+      image: destination,
+    },
+    {
+      id: 30,
+      name: "Botswana",
+      continent: "Africa",
+      country: "Botswana",
       image: destination,
     },
   ]);
+
+  const [selectedDestinations, setSelectedDestinations] = useState<
+    Destination[]
+  >([]);
+
+  useEffect(() => {
+    console.log(selectedDestinations);
+  }, [selectedDestinations]);
 
   return (
     <main className="flex min-h-screen flex-col items-center  p-md-24 p-3 bg-bluesky">
@@ -222,13 +281,46 @@ export default function Destinations() {
       </div>
 
       {/* destinations list */}
-      <div className="grid grid-cols-6 gap-3 w-2/3 mt-4">
+      <div className="grid grid-cols-12 gap-3 w-2/3 mt-4">
         {destinations.map((item, index) => {
           return (
             <div
               key={index}
-              className="xl:col-span-1 md:col-span-2 sm:col-span-3 col-span-6 m-auto cursor-pointer"
+              className="2xl:col-span-2 xl:col-span-3 md:col-span-4 sm:col-span-6 col-span-12 m-auto cursor-pointer relative"
+              onClick={() => {
+                setSelectedDestinations((prevSelectedDestinations) => {
+                  if (
+                    prevSelectedDestinations.some(
+                      (destination) => destination.id === item.id
+                    )
+                  ) {
+                    return prevSelectedDestinations.filter(
+                      (destination) => destination.id !== item.id
+                    );
+                  } else {
+                    return [...prevSelectedDestinations, item];
+                  }
+                });
+              }}
             >
+              <div className="destination-name-overlay flex items-center justify-center">
+                <div className="text-ice text-center poppins-medium">
+                  {item.name}
+                </div>
+              </div>
+
+              {selectedDestinations.some(
+                (destination) => destination.id === item.id
+              ) && (
+                <>
+                  <div className="destination-selected-overlay flex items-center justify-center">
+                    <div className="text-ice text-center poppins-medium">
+                      {"SS"}
+                    </div>
+                  </div>
+                </>
+              )}
+
               <Image
                 className="rounded-[15px]"
                 src={item.image}
