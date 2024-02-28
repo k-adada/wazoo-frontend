a9c6d301af136f2566298a0677a0c32a7626b8af
acsalameh <acsalameh@gmail.com>
2024-02-28 15:19:47 +0200

    fonts linked

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import "./dist/custom.css";
 import "./dist/bootstrap-grid.css";
 import { Providers } from "./providers";
 
-
 export const metadata: Metadata = {
   title: "Lux | Your single Destination",
   description:
@@ -21,13 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
+        <meta
+          property="og:image"
+          content="https://assets.unileversolutions.com/v1/35431836.png"
         />
-         <meta property="og:image" content="https://assets.unileversolutions.com/v1/35431836.png" />
       </Head>
 
       <body className={"poppins-regular"}>
