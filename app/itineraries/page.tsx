6812a3734ebafa311126825d6c4e9fcb3287ba24
acsalameh <acsalameh@gmail.com>
2024-03-13 12:19:39 +0200

    added days to the itinerary

--- app/itineraries/page.tsx
+++ app/itineraries/page.tsx
@@ -12,18 +12,20 @@ import notificationIcon from "../assets/Icon_Notification_Received.png";
 import avatarIcon from "../assets/avatar.png";
 import addIcon from "../assets/Icon_Add.png";
 import backIcon from "../assets/Icon_Back.png";
+import arrowIcon from "../assets/down_arrow_dark.png";
 import serviceEmptyIcon from "../assets/services_empty.png";
-import itineraryIcon from "../assets/Icon_Build_Package.png";
-import reportIcon from "../assets/Icon_Invoice.png";
 import listIcon from "../assets/Icon_List_View.png";
 import ServicesSwiper from "../components/servicesSwiper";
 import locationIcon from "../assets/Icon_Location.png";
 import clockIcon from "../assets/Icon_Time.png";
 import calendarIcon from "../assets/Icon_Calendar_dark.png";
 import dollarIcon from "../assets/Icon_Dollars.png";
-import userIcon from "../assets/Icon_My_Account_dark.png";
-import phoneIcon from "../assets/phone_icon.png";
-import searchIcon from "../assets/Icon_Search_Black.png";
+import louvreImage from "../assets/images/services/Louvre.jpg";
+import lucerneImage from "../assets/images/services/lucerne.jpg";
+import montsaintImage from "../assets/images/services/montsaintmich.jpg";
+import triomphImage from "../assets/images/services/Triomphe.jpg";
+import notreImage from "../assets/images/services/NotreDameCathedral.jpg";
+import quartierImage from "../assets/images/services/Quartier_Latin.jpg";
 
 export default function CreateItineraries() {
   const router = useRouter();
@@ -31,6 +33,97 @@ export default function CreateItineraries() {
     router.back();
   };
   const [itineraries, setItineraries] = useState([1]);
+
+  const [days, setDays] = useState([
+    {
+      id: 1,
+      notesCollapsed: false,
+      collapsed: false,
+      services: [
+        {
+          id: "1",
+          serviceType: "Louvre",
+          description: "Service 1 Description",
+          location: "Location 1",
+          method: "Method 1",
+          duration: "Duration 1",
+          timeslot: "Timeslot 1",
+          price: "350",
+          image: louvreImage,
+        },
+        {
+          id: "2",
+          serviceType: "Lucerne",
+          description: "Service 2 Description",
+          location: "Location 2",
+          method: "Method 2",
+          duration: "Duration 2",
+          timeslot: "Timeslot 2",
+          price: "350",
+          image: lucerneImage,
+        },
+      ] as any[],
+    },
+    {
+      id: 2,
+      notesCollapsed: false,
+      collapsed: true,
+      services: [
+        {
+          id: "5",
+          serviceType: "Notre Dame",
+          description: "Service 5 Description",
+          location: "Location 5",
+          method: "Method 5",
+          duration: "Duration 5",
+          timeslot: "Timeslot 5",
+          price: "350",
+          image: notreImage,
+        },
+        {
+          id: "6",
+          serviceType: "Quartier",
+          description: "Service 6 Description",
+          location: "Location 6",
+          method: "Method 6",
+          duration: "Duration 6",
+          timeslot: "Timeslot 6",
+          price: "350",
+          image: quartierImage,
+        },
+      ] as any[],
+    },
+    {
+      id: 3,
+      notesCollapsed: false,
+      collapsed: false,
+      services: [
+        {
+          id: "3",
+          serviceType: "Mont Saint",
+          description: "Service 3 Description",
+          location: "Location 3",
+          method: "Method 3",
+          duration: "Duration 3",
+          timeslot: "Timeslot 3",
+          price: "350",
+          image: montsaintImage,
+        },
+        {
+          id: "4",
+          serviceType: "Triomphe",
+          description: "Service 4 Description",
+          location: "Location 4",
+          method: "Method 4",
+          duration: "Duration 4",
+          timeslot: "Timeslot 4",
+          price: "350",
+          image: triomphImage,
+        },
+      ] as any[],
+    },
+  ]);
+
   return (
     <main className="flex min-h-screen flex-col items-center p-md-24 p-3 bg-lightblue pb-[100px]">
       <div className="container-fluid">
@@ -159,9 +252,7 @@ export default function CreateItineraries() {
                         </div>
                         <div className="col-auto px-2 py-1">
                           <div className="service-tag flex">
-                            <div className="text-grey">
-                              Outdoor Activity
-                            </div>
+                            <div className="text-grey">Outdoor Activity</div>
                           </div>
                         </div>
                         <div className="col-auto px-2 py-1">
@@ -190,7 +281,7 @@ export default function CreateItineraries() {
                 <div className="col-12 px-2">
                   <div className="f-20 text-grey py-4">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Veniam 
+                    Veniam
                   </div>
                 </div>
 
@@ -214,32 +305,74 @@ export default function CreateItineraries() {
                     <div className="text-grey">Oct 11 - Oct 15</div>
                   </div>
                 </div>
-      
+                <div className="col-12">
+                  {days.map((day, index) => {
+                    return (
+                      <div key={index}>
+                        <div
+                          className="cursor-pointer rounded-lg py-2 my-2 px-2 bg-lightblue flex items-center justify-between"
+                          onClick={() => {
+                            let temp = [...days];
+                            temp[index].collapsed = !temp[index].collapsed;
+                            setDays(temp);
+                          }}
+                        >
+                          <div className="f-24 text-darkblue poppins-semibold">
+                            Day {day.id}
+                          </div>
 
-                <div className="col-12 px-2">
-                  <div className="f-20 poppins-semibold text-darkblue py-2">
-                    Service Provider
-                  </div>
-                  <div className="flex py-1">
-                    <div className="">
-                      <Image src={userIcon} alt="userIcon" />
-                    </div>
-                    <div className="text-darkblue">Le Touriste</div>
-                  </div>
-                  <div className="flex py-1">
-                    <div className="">
-                      <Image src={dollarIcon} alt="dollars" />
-                    </div>
-                    <div className="text-darkblue">$350</div>
-                  </div>
-                  <div className="flex py-1">
-                    <div className="">
-                      <Image src={phoneIcon} alt="phoneIcon" />
-                    </div>
-                    <div className="text-darkblue">
-                      <a href="tel:+9613123456">+33 1 09 75 83 51</a>
-                    </div>
-                  </div>
+                          <div className="">
+                            <Image src={arrowIcon} alt="arrow" />
+                          </div>
+                        </div>
+                        <div
+                          className={`${
+                            day.collapsed ? "hidden" : ""
+                          } col-12 py-2`}
+                        >
+                          {day.services.map((service, index) => {
+                            return (
+                              <div
+                                className="col-12 py-2"
+                                key={index + service.location}
+                              >
+                                <div className="flex py-2 px-2 bg-grey rounded-lg items-center justify-between">
+                                  <div className="flex">
+                                    <Image
+                                      src={service.image}
+                                      alt="tour image"
+                                      className="rounded-lg"
+                                      style={{
+                                        width: "70px",
+                                        height: "70px",
+                                        objectFit: "cover",
+                                      }}
+                                    />
+                                    <div className="pl-3">
+                                      <div className="f-20 text-darkblue poppins-medium">
+                                        {service.serviceType}
+                                      </div>
+                                      <div className="items-center">
+                                        <div className="f-14 text-grey line-clamp-2">
+                                          Lorem ipsum dolor sit amet consectetur
+                                          adipisicing elit. Excepturi nisi
+                                          fugiat inventore iusto architecto,
+                                          similique, voluptas eum culpa neque
+                                          saepe tempore, quo magnam! Molestias
+                                          mollitia ducimus voluptas nihil
+                                          excepturi labore!
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
