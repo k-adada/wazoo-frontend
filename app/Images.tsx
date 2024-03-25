1261b3bbafcd6e7e163262abec7a9e54ce9bba7b
acsalameh <acsalameh@gmail.com>
2024-03-25 14:48:19 +0200

    added catalogue images

--- app/Images.tsx
+++ app/Images.tsx
@@ -2,10 +2,33 @@
 import logo from "@/app/assets/middle_east_yours.png";
 import avatarIcon from "@/app/assets/avatar.png";
 import threeDotsWhiteIcon from "@/app/assets/three_dots_white.png";
+import japan from "@/app/assets/images/catalogues/Japan.webp";
+import kayak from "@/app/assets/images/catalogues/Kayaking.jpg";
+import machupichu from "@/app/assets/images/catalogues/MachuPichu.webp";
+import safari from "@/app/assets/images/catalogues/Safari.webp";
+import santorini from "@/app/assets/images/catalogues/Santorini.webp";
+import skii from "@/app/assets/images/catalogues/Ski.webp";
+import thailand from "@/app/assets/images/catalogues/Thailand.jpg";
+import utah from "@/app/assets/images/catalogues/Utah.jpg";
+import istanbul from "@/app/assets/images/catalogues/istanbul.jpg";
+import aurora from "@/app/assets/images/catalogues/AuroraBorealis.jpg";
+import tajmahal from "@/app/assets/images/catalogues/TajMahal.jpg";
+
 const Images = {
   logo,
   avatarIcon,
   threeDotsWhiteIcon,
+  japan,
+  kayak,
+  machupichu,
+  safari,
+  santorini,
+  skii,
+  thailand,
+  utah,
+  istanbul,
+  aurora,
+  tajmahal,
 };
 
 export default Images;
